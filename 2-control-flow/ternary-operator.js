96b83028cfb5b4735c806bb9e0f27e51a6047e02
Mahesh9359 <maheshjadhav9359@gmail.com>
2025-07-01 19:39:40 +0530

    Add Control-Flow Topics: if-else, for-loops, while-loop, do-while loop, switch case, ternary-operator

--- 2-control-flow/ternary-operator.js
+++ 2-control-flow/ternary-operator.js
@@ -1,7 +1,7 @@
 // 1. Basic ternary
 let age = 20;
 let canVote = age >= 18 ? "Yes" : "No";
-console.log(canVote); // Yes
+console.log(canVote); //     Yes
 
 //Ternary in function return
 function isEven(num) {
